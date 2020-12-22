6f9256156d0c7a4a2a1a17a871f9cc52b5d9b304
MateuszDobosz <mateusz.dobosz3@gmail.com>
2020-12-22 18:28:08 +0100

    readLogs fix

--- src/utils/readLogs.ts
+++ src/utils/readLogs.ts
@@ -1,7 +1,7 @@
 export const splitLine = (line: string) => {
     const splitedLine = line.split(' ');
-    // const timestamp = new Date(parseInt(splitedLine[0].slice(1, -1)));
-    const timestamp = splitedLine[0].slice(1, -1);
+    const timestamp = new Date(parseInt(splitedLine[0].slice(1, -1)));
+    // const timestamp = splitedLine[0].slice(1, -1);
     const uuid = splitedLine[1].slice(1, -1);
     const type = splitedLine[2].slice(1, -1);
     const message = splitedLine.slice(3).join(' ');
