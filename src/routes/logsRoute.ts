5f71ff801cfa3d026fa42c128e188e62378e831c
MateuszDobosz <mateusz.dobosz3@gmail.com>
2020-12-22 16:05:20 +0100

    fix

--- src/routes/logsRoute.ts
+++ src/routes/logsRoute.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { getLogById, getLogs } from '../controllers/log.controller';
+import { verifyRead } from '../middlewares/auth';
 
 let router = express.Router();
 
-router.get('/:id', getLogById);
-router.get('/', getLogs);
+router.get('/:id', verifyRead, getLogById);
+router.get('/', verifyRead, getLogs);
 
 export default router;
